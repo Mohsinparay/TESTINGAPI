1c06d053988c7d2e8dfa0a4fd5f0ce2a61836d76
Mohsin Paray <75415689+Mohsinparay@users.noreply.github.com>
2024-08-20 12:07:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const connectdb = require('./db/connect') 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const products_routes = require('./routes/products')
 app.get("/",(req, res)=>{
-  res.send("I am running");
+  res.send("App is running");
 });
 
 app.use("/api/products",products_routes);
@@ -14,7 +14,7 @@ const start = async()=>{
   try {
     await connectdb(process.env.MONDODB_URL);
     app.listen(PORT,()=>{
-      console.log(`i am running on port ${PORT}`)
+      console.log(`I am running on port ${PORT}`)
     });
   } catch (error) {
     console.log(error);
